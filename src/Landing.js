a21edfedcacf482d03b9cc5bde4c60f160de1678
Mateo Sam <sammateo4@gmail.com>
2021-04-23 19:55:34 -0400

    Remove default formatting

---
+++ src/Landing.js
@@ -0,0 +1,7 @@
+export default function Landing() {
+  return (
+    <div>
+      <h1>Sophia Bygrave-Sam</h1>
+    </div>
+  );
+}
---
+++ src/Post.js
@@ -0,0 +1,10 @@
+function Post() {
+  return (
+    <div>
+      <div>Title</div>
+      <div>Body</div>
+    </div>
+  );
+}
+
+export default Post;
